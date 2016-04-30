302f394a96a63aef28a41d9267aa327cfe43a26c
Jun Sun <qingyun95@gmail.com>
2016-04-30 22:25:47 +1000

    first commit with DocxConverter

---
+++ DocxConverter/DocxConverter/Program.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Reflection;
+
+namespace DocxConverter
+{
+  class Program
+  {
+    static IEnumerable<Tuple<string, WordprocessingDocument>> getDocs(string dir)
+    {
+      var asset_folder = System.IO.Path.Combine(dir, "asset");
+      System.IO.Directory.CreateDirectory(asset_folder);
+      foreach (var file in System.IO.Directory.GetFiles(dir))
+      {
+        yield return Tuple.Create(
+          file, 
+          WordprocessingDocument.Open(file, false));
+      }
+      yield break;
+    }
+
+    static JArray parseParagraph(Paragraph p, IEnumerable<HyperlinkRelationship> hyperlinks)
+    {
+      var text_elms = new string[]{ "r", "hyperlink" };
+
+      var items = p.ChildElements
+        .Where(elm => text_elms.Contains(elm.LocalName))
+        .Aggregate(new List<object>(),
+          (acc, cur) =>
+          {
+            var last = acc.LastOrDefault();
+            if (cur.LocalName == "r")
+            {
+              if (last is string)
+              {
+                acc[acc.Count - 1] = last + cur.InnerText;
+              }
+              else
+              {
+                acc.Add(cur.InnerText);
+              }
+            }
+            else if (cur.LocalName == "hyperlink")
+            {
+              acc.Add(new
+              {
+                text = cur.InnerText,
+                target = hyperlinks.Where(h => h.Id.Equals(((Hyperlink)cur).Id)).First().Uri.AbsoluteUri
+              });
+            }
+            return acc;
+          })
+        .Select(item => {
+          if (item is string)
+          {
+            return new
+            {
+              type = "text",
+              content = item
+            };
+          }
+          else
+          {
+            return new {
+              type = "hyperlink",
+              content = item
+            };
+          }
+        });
+
+      if (items.Count() == 1 && items.Last().type == "text")
+      {
+        var v = (string)items.Last().content;
+        if (v.Trim().Length == 0)
+        {
+          return null;
+        }
+      }
+
+      return items.Aggregate(new JArray(), (arr, item) => {
+          arr.Add(JObject.FromObject(item));
+          return arr;
+        });
+    }
+
+    static string takeParagraphContentStartsWith(ref List<JArray> paragraphs, string to_search)
+    {
+      var target = paragraphs.SingleOrDefault(
+        p => p.Count == 1
+              && p.First.Value<string>("type") == "text"
+              && p.First.Value<string>("content").StartsWith(to_search, StringComparison.CurrentCultureIgnoreCase));
+      paragraphs.Remove(target);
+      return target.First.Value<string>("content");
+    }
+
+    static R protectOn<R>(string file, string part, Func<R> getDefault, Func<R> action)
+    {
+      try
+      {
+        return action();
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine("Got some problems with [{0}] in file [{1}]: {2}", part, file, e.Message);
+        return getDefault();
+      }
+    }
+
+    static JObject parseDoc(Tuple<string, WordprocessingDocument> pair)
+    {
+      var file = pair.Item1;
+      var doc = pair.Item2;
+      var file_name = System.IO.Path.GetFileName(file);
+      var file_path = System.IO.Path.GetDirectoryName(file);
+      var main_part = doc.MainDocumentPart;
+
+      var paragraphs = protectOn(file_name, "description", () => new List<JArray>(),
+        () => 
+        {
+          var hyperlinks = main_part.HyperlinkRelationships;
+          return main_part.Document.Body.OfType<Paragraph>()
+            // remove empty lines
+            .Where(p => p.HasChildren == true || p.InnerText.Length != 0)
+            .Select(p => parseParagraph(p, hyperlinks))
+            // remove image-only paragraphs
+            .Where(p => p != null)
+            .ToList();
+        });
+
+      var tags = protectOn(file_name, "tag", () => new string[] { },
+        () => 
+        {
+          return takeParagraphContentStartsWith(ref paragraphs, "tag:")
+            // remove "tag:"
+            .Substring(4)
+            .Split(',')
+            .Select(w => w.Trim().ToUpper())
+            .Where(w => w != "");
+        });
+
+      var name = protectOn(file_name, "name", () => "",
+        () => 
+        {
+          return takeParagraphContentStartsWith(ref paragraphs, "name:")
+            // remove "name:"
+            .Substring(5)
+            .Trim();
+        });
+
+      var index = protectOn(file_name, "index", () => 0,
+        () => 
+        {
+          return int.Parse(
+            takeParagraphContentStartsWith(ref paragraphs, "index:")
+              // remove "index:"
+              .Substring(6)
+              .Trim());
+        });
+
+      var images = protectOn(file_name, "images", () => new JArray(),
+        () =>
+        {
+          if (name == "") return new JArray();
+
+          var obj_name = name.Replace(' ', '_');
+          var image_parts = main_part.ImageParts;
+          var own_folder = System.IO.Path.Combine(file_path, "asset", obj_name);
+          System.IO.Directory.CreateDirectory(own_folder);
+          return image_parts
+            .Select(
+              img =>
+              {
+                var img_name = System.IO.Path.GetFileName(img.Uri.OriginalString);
+                var img_path = System.IO.Path.Combine(own_folder, img_name);
+                var img_obj = System.Drawing.Image.FromStream(img.GetStream());
+                img_obj.Save(img_path);
+                return new {
+                  type = "image",
+                  url = obj_name + "/" + img_name,
+                  contentType = img.ContentType,
+                  height = img_obj.Height,
+                  width = img_obj.Width
+                };
+              })
+            .Aggregate(new JArray(),
+              (arr, img) => 
+              {
+                arr.Add(JObject.FromObject(img));
+                return arr;
+              });
+            
+        });
+
+      var obj = JObject.FromObject(new {
+        name = name,
+        index = index,
+        tags = tags,
+        description = paragraphs
+          .Aggregate(new JArray(),
+            (arr, p) =>
+            {
+              arr.Add(JObject.FromObject(new
+              {
+                type = "paragraph",
+                content = p
+              }));
+              return arr;
+            }),
+        attachments = images
+      });
+
+      return obj;
+    }
+
+    static JObject formResultJSON(JObject obj, JObject doc)
+    {
+      obj.Value<JArray>("tags").Merge(doc.Value<JArray>("tags"), 
+        new JsonMergeSettings
+        {
+          MergeArrayHandling = MergeArrayHandling.Union
+        });
+
+      obj.Value<JArray>("objects").Add(doc);
+
+      return obj;
+    }
+
+    static void compileDirectory(string dir)
+    {
+      var result_json = getDocs(dir)
+        .Select(pair => parseDoc(pair))
+        .Aggregate(
+          JObject.FromObject(new
+          {
+            tags = new JArray(),
+            objects = new JArray()
+          }), formResultJSON);
+
+      using (var fs = System.IO.File.Open(System.IO.Path.Combine(dir, "asset", "data.json"), System.IO.FileMode.OpenOrCreate))
+      using (var sw = new System.IO.StreamWriter(fs))
+      using (var jw = new JsonTextWriter(sw))
+      {
+        jw.Formatting = Formatting.Indented;
+        var serializer = new JsonSerializer();
+        serializer.Serialize(jw, result_json);
+      }
+    }
+
+    static void Main(string[] args)
+    {
+      compileDirectory(args[0]);
+
+      Console.WriteLine("Press enter to close...");
+      Console.ReadLine();
+    }
+  }
+}
